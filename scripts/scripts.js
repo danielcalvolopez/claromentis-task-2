60ee7b58c6ad8ddd0a61b3e69c832b166fc3d12c
Daniel <acidistrict@gmail.com>
2022-12-21 13:00:55 +0000

    use map function to dynamically render rates

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,15 +2,32 @@ const submitButton = document.getElementById("submit");
 const ratingContainer = document.querySelector(".rating-container");
 const thankyouContainer = document.querySelector(".thankyou-container");
 const rating = document.getElementById("rating");
-const rates = document.querySelectorAll(".button-rating");
+
+const ratings = [
+  { number: "1", id: "1" },
+  { number: "2", id: "2" },
+  { number: "3", id: "3" },
+  { number: "4", id: "4" },
+  { number: "5", id: "5" },
+];
+
+const btnsContent = document.querySelector(".btns-content");
+
+const displayRatings = ratings.map((rating) => {
+  return `<button class="button-rating">${rating.number}</button>`;
+});
+
+btnsContent.innerHTML = displayRatings.join("");
 
 submitButton.addEventListener("click", () => {
   thankyouContainer.classList.remove("hidden");
   ratingContainer.classList.add("hidden");
 });
 
+const rates = document.querySelectorAll(".button-rating");
+
 rates.forEach((rate) => {
-  rate.addEventListener("click", () => {
-    rating.innerHTML = rate.innerHTML;
+  rate.addEventListener("click", (data) => {
+    rating.innerHTML = data.target.innerHTML;
   });
 });
---
+++ utils/constants/ratings.js
@@ -0,0 +1,7 @@
+const ratings = [
+  { name: "1" },
+  { name: "2" },
+  { name: "3" },
+  { name: "4" },
+  { name: "5" },
+];
