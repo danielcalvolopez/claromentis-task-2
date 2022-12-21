effdeea841cbd3c0f2a9a6c30b1927d44c763cee
Daniel <acidistrict@gmail.com>
2022-12-21 01:15:52 +0000

    challenge finished

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,7 +4,17 @@ const thankyouContainer = document.querySelector(".thankyou-container");
 
 const submitButton = document.getElementById("submit");
 
+const rating = document.getElementById("rating");
+
+const rates = document.querySelectorAll(".button-rating");
+
 submitButton.addEventListener("click", () => {
   thankyouContainer.classList.remove("hidden");
   ratingContainer.classList.add("hidden");
+});
+
+rates.forEach((rate) => {
+  rate.addEventListener("click", () => {
+    rating.innerHTML = rate.innerHTML;
+  });
 });
